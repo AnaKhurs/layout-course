fc28a96d3b4d565d60cc46fe150af8fa0efd330b
AnaKhurs <ana.khurs@gmail.com>
2022-05-29 17:03:35 +0300

    add slider landing marketing

--- marketing/js/main.js
+++ marketing/js/main.js
@@ -2,4 +2,10 @@ $(function () {
 
     const mixer = mixitup('.portfolio__content');
 
+    $('.slider-blog__inner').slick({
+        dots: true,
+        prevArrow: '<button type="button" class="slick-prev">            <img src="./img/arrow-left.svg" alt="arrow"></button>',
+        nextArrow: '<button type="button" class="slick-next">            <img src="./img/arrow-right.svg" alt="arrow"></button>',
+    })
+
 })
