76af0684c39c88084a3e22c6553408a47bd049c7
AnaKhurs <ana.khurs@gmail.com>
2022-06-05 20:20:54 +0300

    header adaptive landing marketing

--- marketing/js/main.js
+++ marketing/js/main.js
@@ -1,11 +1,17 @@
 $(function () {
 
-    const mixer = mixitup('.portfolio__content');
-
     $('.slider-blog__inner').slick({
         dots: true,
-        prevArrow: '<button type="button" class="slick-prev">            <img src="./img/arrow-left.svg" alt="arrow"></button>',
-        nextArrow: '<button type="button" class="slick-next">            <img src="./img/arrow-right.svg" alt="arrow"></button>',
-    })
+        prevArrow: '<button type="button" class="slick-prev"><img src="./img/arrow-left.svg" alt="arrow"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="./img/arrow-right.svg" alt="arrow"></button>',
+    });
+
+    $('.menu__btn').on('click', function () {
+        $('.header__top-inner').toggleClass('header__top-inner--active');
+
+    });
+
+
+    const mixer = mixitup('.portfolio__content');
 
 })
