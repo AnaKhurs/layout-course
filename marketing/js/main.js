1237d17480f6fcb45a2739537b88fcd2b7756e0d
AnaKhurs <ana.khurs@gmail.com>
2022-06-09 20:39:02 +0300

    Slider adaptive landing marketing

--- marketing/js/main.js
+++ marketing/js/main.js
@@ -4,6 +4,14 @@ $(function () {
         dots: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="./img/arrow-left.svg" alt="arrow"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="./img/arrow-right.svg" alt="arrow"></button>',
+        responsive: [
+            {
+                breakpoint: 769,
+                settings: {
+                    arrows: false,
+                }
+            },
+        ]
     });
 
     $('.menu__btn').on('click', function () {
